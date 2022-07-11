e68f7dd0e667b515c508e516e6da5368c91c4b4b
Erick Andrade <aguilakrakatoa@gmail.com>
2022-07-11 00:10:11 -0400

    Toggle line comment because nextjs can't run the command build

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,27 +1,26 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
-import { SessionProvider } from "next-auth/react"
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import { Provider } from "react-redux";
 import store from "redux/store";
 import Layout from "components/layout";
 
-
-function MyApp({ Component, pageProps: { session, ...pageProps }, }: AppProps) {
+function MyApp({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   return (
     <>
-    <Head>
-      <title>Leather Store</title>
-    </Head>
-<SessionProvider session={session}>  
-<Provider store={store}>
-<Layout>
-    <Component {...pageProps} />
-</Layout>
-</Provider>
-</SessionProvider>
+      <Head>
+        <title>Leather Store</title>
+      </Head>
+      <SessionProvider session={session}>
+        <Provider store={store}>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </Provider>
+      </SessionProvider>
     </>
-    )
+  );
 }
 
-export default MyApp
+export default MyApp;
